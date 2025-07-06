b41c115a21e1374b91d639a93f01d9dac8af5b9d
Sihang Qiu <33344405+qiusihang@users.noreply.github.com>
2025-07-06 16:58:45 +0800

    update the survey

--- survey.js
+++ survey.js
@@ -2,33 +2,48 @@ var survey = [
     {
         "id": "intro",
         "messages": [
-            "Hi, I'm Andrea. I created this survey to understand if innocent online participants were involved into suspicious activities, \
-            or studies that makes you uncomfortable. We want to know if you have a healthy working environment on the {platform}.",
+            "Hi, I'm Andrea. I created this survey to understand if online participants were involved in malicious or suspicious studies \
+            that makes you uncomfortable. We want to know if you have a healthy working environment on the {platform}.",
             "First of all, let me ask you some questions related to your background.",
             "buttons-only:#OK, no problem."
         ]
     }, {
+        "messages": ["May I know your gender?","buttons-only:#Non-binary#Female#Male#Other#"],
+        "validation": "radio"
+    }, {
+        "messages": ["How old are you?","buttons-only:#younger than 18#18-25#26-35#36-45#46-55#56-65#older than 65#"],
+        "validation": "radio"
+    }, {
+        "messages": ["What is the highest level of education you have completed?",
+            "buttons-only:#Associate's Degree#Bachelor's Degree#Master's Degree#Doctorate (Ph.D., Ed.D., M.D., J.D., etc.)#Some College, No Degree#High School Diploma/GED#Less than High School#Vocational/Technical School Certificate#Other"],
+        "validation": "radio"
+    }, {
+        "messages": ["<img src=\"./res/pam.jpg\" width=\"400px\" style=\"max-width:100%\"/>","In what mood are you today? Select the figure that best represents your current mood.","buttons-only:#A#B#C#D#E#F#G#H#I#"],
+        "validation": "radio"
+    }, {
         "id": "opening",
         "messages": [
-            "Have you experienced some suspicious studies (probably relates to illegal or immoral activities)?",
+            "Have you experienced some malicious or suspicious studies (probably related to illegal or immoral activities)?",
             "buttons-only:#Yes#No"
-        ]
+        ],
+        "validation": "radio"
     }, {
         "id": "times",
         "messages": [
-            "Can you recall how many times have you experienced such studies? If you remember the exact number, you can also type it in the messagebox and send it to me.",
-            "buttons-only:#1-3#4-6#7-9#>=10"
+            "Can you recall how many times you have experienced such studies? If you remember the exact number, please type it in the messagebox and send it to me.",
+            "buttons:#1-3#4-6#7-9#>=10"
         ]
     }, {
         "id": "frequency",
         "messages": [
             "How often have you experienced such studies?",
             "buttons-only:#More than once a week#Every week#Every month#Every year#Less than once a year"
-        ]
+        ],
+        "validation": "radio"
     }, {
         "id": "suspicious",
         "messages": [
-            "Can you describe the suspicious study you remember the most clearly? What did it ask you to do?"
+            "Can you describe a malicious/suspicious study you remember the most clearly? What did it ask you to do?"
         ]
     }, {
         "id": "suspicious_repeat",
@@ -38,20 +53,22 @@ var survey = [
     }, {
         "id": "suspicious2",
         "messages": [
-            "Why do you think it was suspicious?"
+            "Why do you think it was malicious/suspicious?"
         ]
     }, {
         "id": "suspicious3",
         "messages": [
-            "Do you recall any other suspicious studies?",
+            "Do you recall any other malicious/suspicious studies?",
             "buttons-only:#Yes#No"
-        ]
+        ],
+        "validation": "radio"
     }, {
         "id": "measures",
         "messages": [
-            "Do you think there are effective measures to report them, to prevent potential harms?",
+            "Do you think there are effective measures to report them, or to prevent potential harms?",
             "buttons-only:#Yes#No"
-        ]
+        ],
+        "validation": "radio"
     }, {
         "id": "measures_yes",
         "messages": [
@@ -62,7 +79,8 @@ var survey = [
         "messages": [
             "Have you used them often?",
             "buttons-only:#always#often#sometimes#rarely#never"
-        ]
+        ],
+        "validation": "radio"
     }, {
         "id": "measures_no",
         "messages": [
